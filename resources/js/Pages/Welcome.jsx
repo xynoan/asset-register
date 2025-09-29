c6fc11f94adaed56f94ceb056199b94bde3c1d61
xynoan <ncmorva@gmail.com>
2025-09-29 08:39:47 +0800

    ui: form for employees

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -15,13 +15,23 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
         <>
             <Head title="Asset Register" />
-            <main class="fs-1">
-                <h1>Asset Register</h1>
+            <main class="d-flex justify-content-center align-items-center flex-column vh-100">
+                <form class="w-50 border border-black rounded-5 p-5">
+                    <p class="fw-bold fs-3">Asset Register</p>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Full Name</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Birth Date</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </form>
+                <footer className="py-16 text-center text-sm text-black dark:text-white/70 mt-2">
+                    Laravel v{laravelVersion} (PHP v{phpVersion})
+                </footer>
             </main>
-
-            <footer className="py-16 text-center text-sm text-black dark:text-white/70">
-                Laravel v{laravelVersion} (PHP v{phpVersion})
-            </footer>
         </>
     );
 }
