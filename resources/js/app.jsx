9880381645b91fb6e9aef2d7fc92cee4d7adf84a
xynoan <ncmorva@gmail.com>
2025-09-29 08:15:39 +0800

    ui: transition from tailwindcss to bootstrap

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,5 +1,7 @@
 import '../css/app.css';
 import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
