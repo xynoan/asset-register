837148090c8631482831b58d5239e5e974b8a3df
xynoan <ncmorva@gmail.com>
2025-09-30 14:34:50 +0800

    refactor: remove unused arrow function

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -20,16 +20,6 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
             }
         });
     };
-    const handleImageError = () => {
-        document
-            .getElementById('screenshot-container')
-            ?.classList.add('!hidden');
-        document.getElementById('docs-card')?.classList.add('!row-span-1');
-        document
-            .getElementById('docs-card-content')
-            ?.classList.add('!flex-row');
-        document.getElementById('background')?.classList.add('!hidden');
-    };
 
     return (
         <>
