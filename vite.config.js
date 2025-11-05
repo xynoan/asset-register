f45f4de5de2bb46309d91a07b0a425c06d7535d5
xynoan <ncmorva@gmail.com>
2025-11-06 06:11:20 +0800

    allow access from other pc with same network

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,22 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
     server: {
         host: '0.0.0.0', // Allow access from network IP
         cors: {
             origin: "*", // Allow all origins
             credentials: true,
         },
+        port: 5173,
         hmr: {
-            host: 'localhost',
+            host: '192.168.100.169',
         },
     },
+    plugins: [
+        laravel({
+            input: ['resources/js/app.jsx'],
+            refresh: true,
+        }),
+        react(),
+    ],
 });
