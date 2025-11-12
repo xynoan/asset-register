3b776093ac1e532d58e8bf2aa79fd09ae44c0aa9
xynoan <ncmorva@gmail.com>
2025-11-12 14:25:03 +0800

    add assets to routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AssetController;
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,4 +13,8 @@ Route::get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('employees', EmployeeController::class);
     Route::post('employees/{id}/restore', [EmployeeController::class, 'restore'])->name('api.employees.restore');
+    
+    // Add asset routes - the controller already supports API requests
+    Route::apiResource('assets', AssetController::class);
+    Route::post('assets/{id}/restore', [AssetController::class, 'restore'])->name('api.assets.restore');
 });
