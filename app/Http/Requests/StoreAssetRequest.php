a5ca6620bc97b5d1c4e2743dfb8a64b729fc453e
xynoan <ncmorva@gmail.com>
2025-11-13 07:47:05 +0800

    utilize use keyword for more clarity

--- app/Http/Requests/StoreAssetRequest.php
+++ app/Http/Requests/StoreAssetRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 class StoreAssetRequest extends FormRequest
 {
@@ -109,10 +111,10 @@ class StoreAssetRequest extends FormRequest
     /**
      * Handle a failed validation attempt.
      */
-    protected function failedValidation(\Illuminate\Contracts\Validation\Validator $validator)
+    protected function failedValidation(Validator $validator)
     {
         if ($this->expectsJson() || $this->is('api/*')) {
-            throw new \Illuminate\Http\Exceptions\HttpResponseException(
+            throw new HttpResponseException(
                 response()->json([
                     'success' => false,
                     'message' => 'Validation failed',
