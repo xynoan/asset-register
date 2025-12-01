4a2252e16de21e1bd15be8ac3974f4c62e4bf2c1
xynoan <ncmorva@gmail.com>
2025-12-01 10:23:00 +0800

    add hover animation for navigation

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -5,9 +5,9 @@ export default function Header({ activePage = 'assets' }) {
         <div className="container">
             <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
                 <div className="col-md-3 mb-2 mb-md-0">
-                    <a href="#" className="d-inline-flex link-body-emphasis text-decoration-none">
+                    <Link href={route('assets.index')} className="d-inline-flex link-body-emphasis text-decoration-none">
                         <img src={route('storage.private', 'kuga_corp_logo-removebg-preview.png')} alt="Logo" className="img-fluid" />
-                    </a>
+                    </Link>
                 </div>
 
                 <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
@@ -30,7 +30,7 @@ export default function Header({ activePage = 'assets' }) {
                 </ul>
 
                 <div className="col-md-3 text-end">
-                    <button type="button" className="btn btn-outline-danger me-2">Login</button>
+                    <button type="button" className="btn btn-outline-danger me-2" style={{ cursor: 'pointer' }}>Login</button>
                 </div>
             </header>
         </div>
