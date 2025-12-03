3a74be9451d674157ce7cd9fbfd94d5d34e53d4a
xynoan <ncmorva@gmail.com>
2025-12-03 13:49:59 +0800

    redirection after login should be asset

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('assets.index', absolute: false));
+        return redirect()->route('assets.index');
     }
 
     /**
