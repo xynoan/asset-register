538fcd452fc5b3bad391b55fb81dff8680c3b432
xynoan <ncmorva@gmail.com>
2025-12-04 10:01:33 +0800

    dont show id on user management taeble

--- resources/js/Pages/Users/Index.jsx
+++ resources/js/Pages/Users/Index.jsx
@@ -54,7 +54,6 @@ export default function Index({ users, flash }) {
                         <table className="table table-striped">
                             <thead>
                                 <tr>
-                                    <th>ID</th>
                                     <th>Name</th>
                                     <th>Email</th>
                                     <th>Role</th>
@@ -65,7 +64,6 @@ export default function Index({ users, flash }) {
                             <tbody>
                                 {users.map((user) => (
                                     <tr key={user.id}>
-                                        <td>{user.id}</td>
                                         <td>{user.name}</td>
                                         <td>{user.email}</td>
                                         <td>
